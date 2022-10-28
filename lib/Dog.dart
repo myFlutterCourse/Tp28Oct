17cebbadddcb1f1133edecafd8fd570503ae5a7b
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-28 10:47:22 +0100

    create list layout

---
+++ lib/Dog.dart
@@ -0,0 +1,15 @@
+class Dog {
+  int id;
+  String name;
+  double age;
+  String gender;
+  String color;
+  double weight;
+  String location;
+  String image;
+  String about;
+  
+  //constructor
+  Dog(this.id, this.name, this.age, this.gender, this.color, this.weight, this.location, this.image, this.about);
+
+}
